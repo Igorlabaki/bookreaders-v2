a532bdfd58f7122d826d1cea84fb32cb8b1f9b03
Igor Goncalo <igorlabakig@gmail.com>
2022-02-07 19:32:39 +0000

    gdf

---
+++ src/components/layout/Header/style.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const HeaderComponent = styled.div`
+
+    
+
+    h1{
+        font-family: 'Lora', serif;;
+    }
+`
---
+++ src/components/layout/index.tsx
@@ -0,0 +1,22 @@
+import {Header} from "../layout/Header";
+import {Aside} from "../layout/Aside";
+import { LayoutContainer } from "./style";
+import { ReactNode } from "react";
+
+interface LayoutProps{
+    children: ReactNode;
+}
+
+export function Layout({children} : LayoutProps){
+    return(
+        <LayoutContainer>
+            <Header/>
+            <div>
+                <Aside/>
+                <div>
+                    {children}
+                </div>
+            </div>
+        </LayoutContainer>
+    )
+}
