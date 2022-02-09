ad4ffdd77320cf3f7c860e8e32e65dfd46d79899
Igor Goncalo <igorlabakig@gmail.com>
2022-02-09 23:25:37 +0000

    dd

--- src/components/Welcome/Modal/index.tsx
+++ src/components/Welcome/Modal/index.tsx
@@ -7,37 +7,43 @@ export default function ModalComponent() {
 
     const {isLoginModalOpen,isRegisterModalOpen,handleCloseLoginModal,handleCloseRegisterModal} = useModalContext()
 
+    function handleTypeModal(){
+        return(
+            <>
+                { isLoginModalOpen ? 
+                    ( <ModalContainer>
+                            <Modal 
+                                isOpen={isLoginModalOpen}  
+                                onRequestClose={handleCloseLoginModal}
+                                overlayClassName="react-modal-auth-overlay"
+                                className="react-modal-auth-content"
+                                ariaHideApp={false}
+                            >
+                                <Form title="Login"/>
+                            </Modal>
+                        </ModalContainer>) 
+                    :
+                    isRegisterModalOpen ? 
+                        <ModalContainer>
+                            <Modal 
+                                isOpen={isRegisterModalOpen}  
+                                onRequestClose={handleCloseRegisterModal}
+                                overlayClassName="react-modal-auth-overlay"
+                                className="react-modal-auth-content"
+                                ariaHideApp={false}
+                            >
+                                <Form title="Register"/>
+                            </Modal>
+                        </ModalContainer>
+                    : ""
+                }
+            </>
+        )
+    }
+
     return (
         <>
-        { isLoginModalOpen ? 
-           ( <ModalContainer>
-                <Modal 
-                    isOpen={isLoginModalOpen}  
-                    onRequestClose={handleCloseLoginModal}
-                    overlayClassName="react-modal-auth-overlay"
-                    className="react-modal-auth-content"
-                    ariaHideApp={false}
-                >
-                    <Form title="Login"/>
-                </Modal>
-            </ModalContainer>) 
-        :
-         isRegisterModalOpen ? 
-            <ModalContainer>
-                <Modal 
-                    isOpen={isRegisterModalOpen}  
-                    onRequestClose={handleCloseRegisterModal}
-                    overlayClassName="react-modal-auth-overlay"
-                    className="react-modal-auth-content"
-                    ariaHideApp={false}
-                >
-                    <Form title="Register"/>
-                </Modal>
-            </ModalContainer>
-        : ""
-        }
+            {handleTypeModal()}
         </>
     )
-    
-    
 }
